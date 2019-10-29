956569fce8612fbc8d2c3388d172b8d5f7953878
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-29 00:06:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,6 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include? ("wow")
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include? ("Tardis")
 end
 
 def using_sort(array)
