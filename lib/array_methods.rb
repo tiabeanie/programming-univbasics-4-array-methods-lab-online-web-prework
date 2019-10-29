b51945328e6c03438281e0d539397bb6832f2d47
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-29 00:10:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-def using_include(array, element, element1)
+def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include? ("wow"),("Tardis")
 end
